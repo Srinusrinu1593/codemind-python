d5f59b22672208c94e37f90430644ffc1438b011
Srinusrinu1593 <96323353+Srinusrinu1593@users.noreply.github.com>
2022-10-11 21:27:03 +0530

    def selfdiv(num):     temp=num     while temp:         d=temp%10         temp=temp//10         if d==0 or num%d!=0:             return False     return True n=int(input()) m=int(input()) for i in range(n,m+1):     if selfdiv(i):         print(i,end=" ")

---
+++ Self_Dividing_Numbers.py
@@ -0,0 +1,13 @@
+def selfdiv(num):
+    temp=num
+    while temp:
+        d=temp%10
+        temp=temp//10
+        if d==0 or num%d!=0:
+            return False
+    return True
+n=int(input())
+m=int(input())
+for i in range(n,m+1):
+    if selfdiv(i):
+        print(i,end=" ")
